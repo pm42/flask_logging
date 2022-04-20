05c55bc5439a73f76da1aa85510a458a07e2bcc7
pm42 <pm42@njit.edu>
2022-04-19 22:13:18 -0400

    Added Info filter

--- app/log_filter/__init__.py
+++ app/log_filter/__init__.py
@@ -1,9 +1,17 @@
 import logging
-
+from flask import has_request_context
 
 class FilterDebug(logging.Filter):
     def filter(self, record):
         if record.levelno == 10:
+            return True
+        else:
+            return False
+
+
+class FilterInfo(logging.Filter):
+    def filter(self, record):
+        if record.levelno == 20 and has_request_context():
             return True
         else:
             return False
